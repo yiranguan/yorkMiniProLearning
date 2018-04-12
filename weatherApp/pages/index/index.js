d5c3788f576003936260b91772c5901883056ac8
York Guan <yiranguan@hotmail.com>
2018-04-12 23:56:52 +0800

    fix: Creat data after lesson

--- weatherApp/pages/index/index.js
+++ weatherApp/pages/index/index.js
@@ -1,7 +1,10 @@
 Page({
   data:{
-    temp:0,
-    weather:'阴天',
+    // temp:0,
+    // weather:'阴天',
+    // 以上自行尝试
+    nowTemp:14,
+    nowWeather:'多云',
   },
   onLoad(){
     console.log('Hello World!');
@@ -13,12 +16,17 @@ Page({
         // console.log('weather: ' + res.data.result.now.weather);
         // console.log('data-now: ' + res.data.result.now);
         // 以上注释掉自己联系的部分，以下学习老师的方法做一次
+        
         let result = res.data.result;
         let temp = result.now.temp;
         let weather = result.now.weather;
+        console.log(temp, weather);
+        /*
         // data.temp = temp;
         // data.weather = weather;
         // 以上两行有问题，需要想办法将temp和weather传送到data的相应位置
+        以上自己尝试 
+        */
       },
     })
   }
