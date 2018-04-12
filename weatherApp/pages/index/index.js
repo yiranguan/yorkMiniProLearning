a58b28156738ebeb9d3d3d61f4135fbfb812e845
York Guan <yiranguan@hotmail.com>
2018-04-12 23:19:43 +0800

    feature:get temp myself

--- weatherApp/pages/index/index.js
+++ weatherApp/pages/index/index.js
@@ -5,7 +5,9 @@ Page({
       url: 'https://test-miniprogram.com/api/weather/now',
       data:{city:'广州市'},
       success:res => {
-        console.log(res.data);
+        console.log('temp：' + res.data.result.now.temp);
+        console.log('weather: ' + res.data.result.now.weather);
+        console.log('data-now: ' + res.data.result.now);
       },
     })
   }
