c450f94add3534ba28d5ee51f02f45b08567816a
York Guan <yiranguan@hotmail.com>
2018-04-13 00:16:35 +0800

    fix: Set data after lesson

--- weatherApp/pages/index/index.js
+++ weatherApp/pages/index/index.js
@@ -1,8 +1,14 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
 Page({
   data:{
-    // temp:0,
-    // weather:'阴天',
-    // 以上自行尝试
     nowTemp:14,
     nowWeather:'多云',
   },
@@ -12,24 +18,13 @@ Page({
       url: 'https://test-miniprogram.com/api/weather/now',
       data:{city:'广州市'},
       success:res => {
-        // console.log('temp：' + res.data.result.now.temp);
-        // console.log('weather: ' + res.data.result.now.weather);
-        // console.log('data-now: ' + res.data.result.now);
-        // 以上注释掉自己联系的部分，以下学习老师的方法做一次
-        
         let result = res.data.result;
         let temp = result.now.temp;
         let weather = result.now.weather;
         console.log(temp, weather);
-        /*
-        // data.temp = temp;
-        // data.weather = weather;
-        // 以上两行有问题，需要想办法将temp和weather传送到data的相应位置
-        以上自己尝试 
-        */
         this.setData({
-          nowTemp: temp,
-          nowWeather: weather
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather]
         });
       },
     })
