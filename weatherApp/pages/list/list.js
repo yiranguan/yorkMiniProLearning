0757fa7e59af75483d5e6c45ea6b670fc76db0f2
York Guan <yiranguan@hotmail.com>
2018-04-15 01:29:37 +0800

    feature: Try build list page my self

--- weatherApp/pages/list/list.js
+++ weatherApp/pages/list/list.js
@@ -1,4 +1,51 @@
 // pages/list/list.js
 Page({
-  
+  data:{
+    dateItem:[
+      
+      {
+        day:'星期一',
+        date:'2018/1/2',
+        temp:'0-20',
+        weather:'/images/sunny-icon.png'
+      },
+      {
+        day: '星期一',
+        date: '2018/1/2',
+        temp: '0-20',
+        weather: '/images/sunny-icon.png'
+      },
+      {
+        day: '星期一',
+        date: '2018/1/2',
+        temp: '0-20',
+        weather: '/images/sunny-icon.png'
+      },
+      {
+        day: '星期一',
+        date: '2018/1/2',
+        temp: '0-20',
+        weather: '/images/sunny-icon.png'
+      },
+      {
+        day: '星期一',
+        date: '2018/1/2',
+        temp: '0-20',
+        weather: '/images/sunny-icon.png'
+      },
+      {
+        day: '星期一',
+        date: '2018/1/2',
+        temp: '0-20',
+        weather: '/images/sunny-icon.png'
+      },
+      {
+        day: '星期一',
+        date: '2018/1/2',
+        temp: '0-20',
+        weather: '/images/sunny-icon.png'
+      },
+
+    ],
+  },
 })
